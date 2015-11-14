84f90bc01c49c98d407ac98ccd25ea98a7f7988a
Jackie Wu <jackielwu@gmail.com>
2015-11-14 14:47:35 -0500

    Initial

---
+++ src/SessionCookie.java
@@ -0,0 +1,13 @@
+/**
+ * CS180 - HW 0
+ * Description:
+ *
+ * @author Jackie Wu, wu891@purdue.edu, 821
+ * @version November 14, 2015
+ */
+public class SessionCookie {
+
+    public boolean hasTimedOut() {
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,9 @@
+/**
+ * CS180 - HW 0
+ * Description:
+ *
+ * @author Jackie Wu, wu891@purdue.edu, 821
+ * @version November 14, 2015
+ */
+public class User {
+}
