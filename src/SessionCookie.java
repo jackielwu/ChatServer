95c0a6cb0455bfce842ff0400c5d53a9647acbd1
Jackie Wu <jackielwu@yahoo.com>
2015-11-14 16:55:35 -0500

    Updated SessionCookie

--- src/SessionCookie.java
+++ src/SessionCookie.java
@@ -6,8 +6,22 @@
  * @version November 14, 2015
  */
 public class SessionCookie {
+    private long id;
+
+    public SessionCookie(long id) {
+        this.id = id;
+    }
+    public static int timeoutlength
 
     public boolean hasTimedOut() {
         return false;
+    }
+
+    public void updateTimeOfActivity() {
+        System.currentTimeMillis();
+    }
+
+    public long getID() {
+        return id;
     }
 }
