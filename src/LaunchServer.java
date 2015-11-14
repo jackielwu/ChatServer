62070def5977a36091f5266334d45e2ad021cb17
Jackie Wu <jackielwu@yahoo.com>
2015-11-14 16:27:07 -0500

    Added MessageFactory

---
+++ src/LaunchServer.java
@@ -0,0 +1,12 @@
+public class LaunchServer {
+
+    /**
+     * This main method is for testing purposes only.
+     * @param args - the command line arguments
+     */
+    public static void main(String[] args) {
+        // Create a ChatServer and start it
+        (new ChatServer(new User[0], 200)).run();
+    }
+
+}
