b7a0cbe7a8d48d3f8fca537b4593e2382cbf6efc
Jackie Wu <jackielwu@gmail.com>
2015-11-14 14:51:42 -0500

    Added User

--- src/User.java
+++ src/User.java
@@ -6,4 +6,29 @@
  * @version November 14, 2015
  */
 public class User {
+    private String username;
+    private String password;
+    private SessionCookie cookie;
+
+    public User(String username, String password, SessionCookie cookie) {
+        this.username = username;
+        this.password = password;
+        this.cookie = cookie;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public boolean checkPassword(String password) {
+        return (password.equals(this.password)) ? true: false;
+    }
+
+    public SessionCookie getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(SessionCookie cookie) {
+        this.cookie = cookie;
+    }
 }
